64744b17cc6a1f1439df8c9ce2d4f08524bdd356
Mark Hernandez <m.hern1415@gmail.com>
2019-10-25 02:11:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,8 +31,13 @@ end
 	end
 
 	post "/login" do
-		  user = User.find_by(:username => params[:username])
-	end
+  user = User.find_by(:username => params[:username])
+  if user
+    redirect "/success"
+  else
+    redirect "/failure"
+  end
+  end
 
 	get "/success" do
 		if logged_in?
